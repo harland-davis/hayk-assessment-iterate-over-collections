35ec48673fb5a8893b205be7eb81e95d31e5fcb5
Kyle Davis <kyledavis@Kyles-iMac.local>
2021-02-08 17:49:07 -0700

    commit tree 50ce8bfc3e59d55848a9c697b7ad4dc3017cbfdf

--- app.rb
+++ app.rb
@@ -14,3 +14,42 @@ companies = [
 ]
 
 ### Your code below this
+
+def name_and_size companies
+  companies.map do |company|
+    company.name + " - " + company.size.to_s
+  end
+end
+
+def sizes_with_reduce companies
+  total_of_sizes = companies.reduce(0) do |sum, company|
+    company.size + sum
+  end
+  total_of_sizes
+end
+
+def companies_over_100 companies
+  companies.filter do |company|
+    company.size > 100
+  end
+end
+
+def company_beta companies
+  companies.find do |company|
+    company.name == 'Beta'
+  end
+end
+
+def largest_company companies
+  companies.max do |company|
+    company.size
+  end
+end
+
+def largest_to_smallest companies 
+  companies.sort do |company|
+    company.size
+  end
+end
+
+
